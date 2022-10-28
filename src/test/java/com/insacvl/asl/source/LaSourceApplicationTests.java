0c11c8d9b99dd6cb1efc8ef03fcb1fb157fc00f6
Hugo Sarazin <hu.sarazin@gmail.com>
2022-10-28 14:09:53 +0200

    Initial commit

---
+++ src/test/java/com/insacvl/asl/source/LaSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insacvl.asl.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
